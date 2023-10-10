ac9b399909843a0f4b9dda7ea3d44dc66a926b42
Ethan Wiegert <ewiegert99@gmail.com>
2023-10-10 18:36:28 -0400

    Initial version with working list function.

--- src/UseMyList.js
+++ src/UseMyList.js
@@ -1,69 +1,85 @@
-import React from 'react';
-import './App.css';
-import { Link, useHistory } from "react-router-dom";
-import Button from 'react-bootstrap/Button';
-import Col from 'react-bootstrap/Col';
-import Form from 'react-bootstrap/Form';
-import InputGroup from 'react-bootstrap/InputGroup';
-import Row from 'react-bootstrap/Row';
-
-import NavigationBar from './NavigationBar';
-//finish implementing a form
+import React from "react";
+import {useState, useEffect} from "react"
+import {useHistory} from "react-router-dom"
+import NavigationBar from "./NavigationBar";
 
 
-function UseMyList() {
-  return (
-    <>
-    <div>
-
-    <div className="row pb-5">
-    <div className="d-flex justify-content-center bg-dark text-white p-5">
-    <h1>Decide Dinner</h1>
+function UseMyList(){
+    let nextId = 0;
     
+
+    const [name, setName] = useState('');
+    const [list, setList] = useState([]);
+
+    
+
+  
+
+    function getRandomInt(max) {
+        return Math.floor(Math.random() * max);
+      }
+      
+      function NewRestaraunt(){
+          return getRandomInt(list.length)
+      }
+  
+    return (
+      <div>
+          <div>
+    <div className="row pb-5">
+      <div className="d-flex justify-content-center bg-dark text-white p-5">
+    <h1>Decide Dinner</h1>
+  
     </div>
     <NavigationBar/>
     </div>
     </div>
+    
+    
+        <h1>Add restaurants:</h1>
+        <input
+          value={name}
+          onChange={e => setName(e.target.value)}
+        />
+        <button onClick={() => {
+          setList([
+            ...list,
+            { id: nextId++, name: name }
+          ]);
+        }}>Add</button>
+        <br/>
+        <h3>Your List:</h3>
+        <ul>
+        {list.map(item => (
+          <li key={item.id}>
+            {item.name}{' '}
+            <button onClick={() => {
+              setList(
+                list.filter(a =>
+                  a.name !== item.name
+                )
+              );
+            }}>
+              Delete
+            </button>
+          </li>
+        ))}
+      </ul>
+      <br/>
+      <h4>Decide from this list</h4>
+      <button>Choose</button>
+     
+ </div>
+
+
+      
+    );
+
 
     
-    <Form>
-      <Row className="align-items-center">
-        <Col xs="auto">
-          <Form.Label htmlFor="inlineFormInput" visuallyHidden>
-            Name
-          </Form.Label>
-          <Form.Control
-            className="mb-2"
-            id="inlineFormInput"
-            placeholder="Jane Doe"
-          />
-        </Col>
-        <Col xs="auto">
-          <Form.Label htmlFor="inlineFormInputGroup" visuallyHidden>
-            Username
-          </Form.Label>
-          <InputGroup className="mb-2">
-            <InputGroup.Text>@</InputGroup.Text>
-            <Form.Control id="inlineFormInputGroup" placeholder="Username" />
-          </InputGroup>
-        </Col>
-        <Col xs="auto">
-          <Form.Check
-            type="checkbox"
-            id="autoSizingCheck"
-            className="mb-2"
-            label="Remember me"
-          />
-        </Col>
-        <Col xs="auto">
-          <Button type="submit" className="mb-2">
-            Submit
-          </Button>
-        </Col>
-      </Row>
-    </Form>
-    </>
-  );
+
+
 }
 
-export default UseMyList;
+
+export default UseMyList
